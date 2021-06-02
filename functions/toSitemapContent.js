0617ad834e56e9008605b11dfab1be6291a3ab74
Your Name <>
2021-06-02 22:57:20 +0200

    fixed robots and sitemap

--- functions/toSitemapContent.js
+++ functions/toSitemapContent.js
@@ -1,10 +1,13 @@
 const toXml = data => {
   var xmlStringFeed = `<?xml version="1.0" encoding="UTF-8"?>
-                          <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`
+                          <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+                          <url>
+                             <loc>https://spoketa.cz/</loc>
+                          </url>
+                        `
 
   const dataTransform = data.reduce((result, item) => {
    return result + `<url>
-      ${result.length == 0 ? '<loc>https://spoketa.cz/</loc>' : ''}
       <loc>https://spoketa.cz${item}</loc>
    </url>`
   }, '')
